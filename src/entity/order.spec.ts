2860361c943f5b42e9b2acf98a87af9f1435a2e4
togo88-global <tiagogb_2@hotmail.com>
2022-08-17 16:51:25 -0300

    error correction

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -23,17 +23,26 @@ describe("Order unit tests", () => {
 
      it("should calculate total", () => {
         
-        const item = new OrderItem("i1", "Item 1", 100);
-        const item2 = new OrderItem("i2", "Item 2", 200);
+        const item = new OrderItem("i1", "Item 1", 100, "p1", 2);
+        const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
         const order = new Order("o1", "c1", [item]);        
 
         let total = order.total();            
         
-        expect(total).toBe(100);
+        expect(total).toBe(200);
 
         const order2 = new Order("o1", "c1", [item, item2]);
         total = order2.total();      
 
-        expect(total).toBe(300);
+        expect(total).toBe(600);
      });
+
+     it("should throw error if the item qtde is less or equal than zero", () => {                
+
+      expect(() => {
+         const item = new OrderItem("i1", "Item 1", 100, "p1", 0);     
+         const order = new Order("o1", "c1", [item]);      
+      }).toThrowError("Quantity must be greater than zero");      
+      
+   });
 });
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address("Rua Teste", 123, "12345-678", "Teste");
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+const item2 = new OrderItem("2", "Item 2", 15, "p1", 2);
 
 const order = new Order("1", "123", [item1, item2]);
